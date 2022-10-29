9b411972f5ba4de369fe30e2ef2d007fd9b96964
Abhishek Raushan <abhik.raushan@gmail.com>
2022-10-29 12:42:34 +0530

    Initial Project Setup
    Initial Project Setup

---
+++ src/main/java/com/abhisu/microservice/AbhisuMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.abhisu.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbhisuMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbhisuMicroserviceApplication.class, args);
+	}
+
+}
